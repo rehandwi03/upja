1438916fa06bdc5378e98c539e96239b8fb42f1f
rehandwi03 <rehandwi2910@gmail.com>
2020-05-15 10:51:58 +0700

    change admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,6 +16,7 @@ class AdminController extends Controller
 
     public function store(Request $request)
     {
+        // dd($request->all());
         $this->validate($request, [
             'id_role' => 'required|string|numeric',
             'admin_username' => 'required|string',
@@ -27,10 +28,41 @@ class AdminController extends Controller
             'admin_username' => $request->admin_username
         ], [
             'id_role' => $request->id_role,
-            'admin_password' => $request->admin_password,
+            'admin_password' => app('hash')->make($request->admin_password),
             'admin_fullname' => $request->admin_fullname,
             'admin_hide' => 0
         ]);
         return response()->json($admin, 201);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'id_admin' => 'required|string|numeric',
+            'id_role' => 'required|string|numeric',
+            'admin_username' => 'required|string',
+            'admin_password' => 'nullable|string',
+            'admin_fullname' => 'required|string'
+        ]);
+
+        $admin = MSAdmin::findOrFail($request->id_admin);
+        $admin->update([
+            'id_role' => $request->id_role,
+            'admin_username' => $request->admin_username,
+            'admin_password' => $request->admin_password,
+            'admin_fullname' => $request->admin_fullname
+        ]);
+        return response()->json($admin, 200);
+    }
+
+    public function hide(Request $request)
+    {
+        $this->validate($request, [
+            'id_admin' => 'required|string|numeric',
+        ]);
+        $admin = MSAdmin::findOrFail($request->id_admin);
+        $admin->update([
+            'admin_hide' => 1
+        ]);
     }
 }
