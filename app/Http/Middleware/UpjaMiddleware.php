9be30d406f9bcb7315f5a10fc197f347c8cc6e3d
rehandwi03 <rehandwi2910@gmail.com>
2020-05-25 11:31:43 +0700

    nambahin middleware upja

---
+++ app/Http/Middleware/UpjaMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class UpjaMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Pre-Middleware Action
+        $role = $request->role_name;
+        if (!$role) {
+            return [
+                'code' => 401,
+                'error' => 'Role not provided.'
+            ];
+        } elseif ($role == "upja") {
+            return $next($request);
+        } else {
+            return [
+                'code' => 400,
+                'error' => 'Unauthorized'
+            ];
+        }
+    }
+}
